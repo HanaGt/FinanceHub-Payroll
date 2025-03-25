064c2de0a3691eaab0c67f80f231f56e6dfd2ca8
HanaGt <hana.guta-ug@aau.edu.et>
2025-03-25 12:14:25 +0300

    change button color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-[#146ef5] text-white hover:bg-[#125ed1]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
